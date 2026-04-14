1fe9206dbc5cf343e125d3e65a0786248ce11f2d
FabricinBR <89689889+FabricinBR@users.noreply.github.com>
2026-04-14 15:00:30 -0300

    Aplica novos links do Drive nas imagens dos produtos

--- script.js
+++ script.js
@@ -11,116 +11,96 @@ const dadosPagamento = document.getElementById('dados-pagamento');
 
 const catalogo = {
   'Eletroportáteis': [
-    ['Air Fryer Mondial Family AFN-40-BI 4L', 'a partir de R$ 353,31 no Pix.'],
-    ['Liquidificador Mondial Turbo Glass L-1400 GI', 'R$ 218,41 no Pix.'],
-    ['Cafeteira Elétrica Mondial Dolce Arome C-30-18X', 'R$ 116,99 no Pix.'],
-    ['Cafeteira Elétrica Mondial Dolce Arome Thermo C-33JT-24X', 'R$ 260,39 no Pix.'],
-    ['Grill e Sanduicheira Mondial Premium S-07', 'R$ 120,81 no Pix.'],
-    ['Batedeira Prática Mondial B-44-B 400W', 'R$ 264,74.'],
-    ['Pipoqueira Elétrica Mondial Popflix PP-04', 'R$ 244,90.'],
-    ['Ferro a Vapor Mondial FVN-01-BL', 'R$ 116,91 no Pix.'],
-    ['Panela de Pressão Elétrica Digital Mondial', 'R$ 379,05 no Pix.']
+    ['Liquidificador', 200],
+    ['Cafeteira', 250],
+    ['Micro-ondas', 600],
+    ['Air Fryer', 400]
   ],
   'Cozinha e mesa': [
-    ['Jogo de Panelas Tramontina Fundo Triplo 3 peças', 'R$ 323,10.'],
-    ['Jogo de Panelas Tramontina Solar 6 peças', 'R$ 709,00.'],
-    ['Jogo de Panelas Tramontina Allegra 5 peças', 'R$ 409,82.'],
-    ['Kit Utensílios de Silicone Wincy 8 peças', 'R$ 169,90.'],
-    ['Kit 10 Utensílios de Silicone para cozinha', 'R$ 127,00.'],
-    ['Decanter Bohemia Fórum', 'R$ 159,99.'],
-    ['Aparelho de Jantar Oxford Ryo Maresia 20 peças', 'a partir de R$ 309,00.']
+    ['Taças de vinho', 200],
+    ['Tábua de frios', 630],
+    ['Jogo americano', 178],
+    ['Saleiro', 148],
+    ['Utensílios inox dourado', 239],
+    ['Utensílios de cozinha', 150],
+    ['Açucareiro', 100],
+    ['Porta manteiga', 156],
+    ['Conjunto de jantar', 145],
+    ['Faqueiro 91 peças', 370]
   ],
   'Cama e banho': [
-    ['Jogo de Cama Queen 4 Peças Naturalle', 'R$ 304,90.'],
-    ['Jogo de Toalhas Buddemeyer Soul Extra Soft 5 peças', 'R$ 229,90.'],
-    ['Travesseiro Buddemeyer Toque de Pluma 50x70 cm', 'R$ 119,90.'],
-    ['Kit Edredom Jogo de Cama Dupla Face 6 peças Queen', 'R$ 218,49.'],
-    ['Jogo de Toalhas Buddemeyer Fio Penteado Canelado Gigante 5 peças', 'R$ 399,90.'],
-    ['Jogo de Toalhas Buddemeyer Florentina Extra Soft 5 peças', 'R$ 159,90.'],
-    ['Jogo de Cama Queen 4 peças 200 fios Organique', 'R$ 351,90.'],
-    ['Edredom Queen Nobre 1 peça dupla face', 'R$ 329,48.']
+    ['Jogo de cama', 200],
+    ['Jogo de toalhas', 190]
   ],
   'Lua de mel': [
-    ['✈️ Passagens aéreas', 'R$ 300 a R$ 1.500 (cotas).'],
-    ['🏨 Hospedagem (diárias)', 'R$ 250 a R$ 800.'],
-    ['🚖 Transfer aeroporto/hotel', 'R$ 200 a R$ 400.'],
-    ['💳 Ajuda geral para viagem', 'R$ 200 a R$ 1.000.'],
-    ['🍷 Jantar romântico especial', 'R$ 200 a R$ 500.'],
-    ['🍾 Noite com vinho/champagne', 'R$ 200 a R$ 400.'],
-    ['🌅 Jantar à beira-mar', 'R$ 300 a R$ 600.'],
-    ['🍳 Café da manhã especial no hotel', 'R$ 200 a R$ 350.']
+    ['Passeio de barco', 500],
+    ['Spa para casal', 300],
+    ['Passagens', 800],
+    ['Diária de hotel', 300]
   ]
 };
 
 const imagemFallback = 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4?auto=format&fit=crop&w=800&q=80';
 const imagensPorProduto = {
-  'Air Fryer Mondial Family AFN-40-BI 4L':
-    'https://images.unsplash.com/photo-1585515656973-3c2b3f978f29?auto=format&fit=crop&w=800&q=80',
-  'Liquidificador Mondial Turbo Glass L-1400 GI':
-    'https://images.unsplash.com/photo-1570222094114-d054a817e56b?auto=format&fit=crop&w=800&q=80',
-  'Cafeteira Elétrica Mondial Dolce Arome C-30-18X':
-    'https://images.unsplash.com/photo-1517701550927-30cf4ba1f26b?auto=format&fit=crop&w=800&q=80',
-  'Cafeteira Elétrica Mondial Dolce Arome Thermo C-33JT-24X':
-    'https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?auto=format&fit=crop&w=800&q=80',
-  'Grill e Sanduicheira Mondial Premium S-07':
-    'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?auto=format&fit=crop&w=800&q=80',
-  'Batedeira Prática Mondial B-44-B 400W':
-    'https://images.unsplash.com/photo-1627485937980-221c88ac04f9?auto=format&fit=crop&w=800&q=80',
-  'Pipoqueira Elétrica Mondial Popflix PP-04':
-    'https://images.unsplash.com/photo-1585647347384-2593bc35786b?auto=format&fit=crop&w=800&q=80',
-  'Ferro a Vapor Mondial FVN-01-BL':
-    'https://images.unsplash.com/photo-1599510686580-0cb72c6f5876?auto=format&fit=crop&w=800&q=80',
-  'Panela de Pressão Elétrica Digital Mondial':
-    'https://images.unsplash.com/photo-1584990347449-ae3b35a4f2a8?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Panelas Tramontina Fundo Triplo 3 peças':
-    'https://images.unsplash.com/photo-1556911220-bff31c812dba?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Panelas Tramontina Solar 6 peças':
-    'https://images.unsplash.com/photo-1584990347449-ae3b35a4f2a8?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Panelas Tramontina Allegra 5 peças':
-    'https://images.unsplash.com/photo-1547592166-23ac45744acd?auto=format&fit=crop&w=800&q=80',
-  'Kit Utensílios de Silicone Wincy 8 peças':
-    'https://images.unsplash.com/photo-1590794056226-79ef3a8147e1?auto=format&fit=crop&w=800&q=80',
-  'Kit 10 Utensílios de Silicone para cozinha':
-    'https://images.unsplash.com/photo-1577048982768-5cb3f10f9f6f?auto=format&fit=crop&w=800&q=80',
-  'Decanter Bohemia Fórum':
-    'https://images.unsplash.com/photo-1516594915697-87eb3b1c14ea?auto=format&fit=crop&w=800&q=80',
-  'Aparelho de Jantar Oxford Ryo Maresia 20 peças':
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Cama Queen 4 Peças Naturalle':
-    'https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Toalhas Buddemeyer Soul Extra Soft 5 peças':
-    'https://images.unsplash.com/photo-1616627547584-bf28cee262db?auto=format&fit=crop&w=800&q=80',
-  'Travesseiro Buddemeyer Toque de Pluma 50x70 cm':
-    'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?auto=format&fit=crop&w=800&q=80',
-  'Kit Edredom Jogo de Cama Dupla Face 6 peças Queen':
-    'https://images.unsplash.com/photo-1616594039964-3cf36f8d5d39?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Toalhas Buddemeyer Fio Penteado Canelado Gigante 5 peças':
-    'https://images.unsplash.com/photo-1584622650111-993a426fbf0a?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Toalhas Buddemeyer Florentina Extra Soft 5 peças':
-    'https://images.unsplash.com/photo-1600369672770-985fd300a37f?auto=format&fit=crop&w=800&q=80',
-  'Jogo de Cama Queen 4 peças 200 fios Organique':
-    'https://images.unsplash.com/photo-1566665797739-1674de7a421a?auto=format&fit=crop&w=800&q=80',
-  'Edredom Queen Nobre 1 peça dupla face':
-    'https://images.unsplash.com/photo-1616628182509-6fcb8c6f8d5d?auto=format&fit=crop&w=800&q=80',
-  '✈️ Passagens aéreas':
-    'https://images.unsplash.com/photo-1436491865332-7a61a109cc05?auto=format&fit=crop&w=800&q=80',
-  '🏨 Hospedagem (diárias)':
-    'https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&w=800&q=80',
-  '🚖 Transfer aeroporto/hotel':
-    'https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?auto=format&fit=crop&w=800&q=80',
-  '💳 Ajuda geral para viagem':
-    'https://images.unsplash.com/photo-1556740749-887f6717d7e4?auto=format&fit=crop&w=800&q=80',
-  '🍷 Jantar romântico especial':
-    'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?auto=format&fit=crop&w=800&q=80',
-  '🍾 Noite com vinho/champagne':
-    'https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?auto=format&fit=crop&w=800&q=80',
-  '🌅 Jantar à beira-mar':
-    'https://images.unsplash.com/photo-1467003909585-2f8a72700288?auto=format&fit=crop&w=800&q=80',
-  '🍳 Café da manhã especial no hotel':
-    'https://images.unsplash.com/photo-1533089860892-a7c6f0a88666?auto=format&fit=crop&w=800&q=80'
+  Liquidificador:
+    'https://drive.google.com/file/d/1geUf3tLHUdM74nhrQlanZqJb5C9_znog/view?usp=sharing',
+  Cafeteira:
+    'https://drive.google.com/file/d/1-bPc1b_F5_tQvw19WOol92hjjeiA3FRT/view?usp=sharing',
+  'Micro-ondas':
+    'https://drive.google.com/file/d/1jdsgskSUrOZQSsREPpunLGXa2DZqEB5e/view?usp=sharing',
+  'Air Fryer':
+    'https://drive.google.com/file/d/1WZPQPegaTCr5iKHJKh5JKPDs0_Cjqz1s/view?usp=sharing',
+  'Taças de vinho':
+    'https://drive.google.com/file/d/1JqgDMPTeTE3fS6QB8FPKmkQQzAPgwxIx/view?usp=sharing',
+  'Tábua de frios':
+    'https://drive.google.com/file/d/1WfkVb-Rsym1ZAJo3BfI-XtrTby_yw_p7/view?usp=sharing',
+  'Jogo americano':
+    'https://drive.google.com/file/d/1whUDHL92mub43Or82KpMAG3R8sAWNYt1/view?usp=sharing',
+  Saleiro:
+    'https://drive.google.com/file/d/1NYFKKIto2Fzp5W09rkn3qJpsg9ZjkkH7/view?usp=sharing',
+  'Utensílios inox dourado':
+    'https://drive.google.com/file/d/1N8REmjLAaEAp7igEgio6gbUDwhuJvN5M/view?usp=sharing',
+  'Utensílios de cozinha':
+    'https://drive.google.com/file/d/1iuFIwKWxLEgoHMh4u8CrdglNjO-8Lub0/view?usp=sharing',
+  Açucareiro:
+    'https://drive.google.com/file/d/1rzosNqkC_eK3RaG6KqGRWf1msWsfi3hp/view?usp=sharing',
+  'Porta manteiga':
+    'https://drive.google.com/file/d/1nsfThvI1XK7eJ9X1rHyfHpRnKQWrxU6N/view?usp=sharing',
+  'Conjunto de jantar':
+    'https://drive.google.com/file/d/1wQ5a8OA9eKi3XYo489QFU5vSOV0cmL8U/view?usp=sharing',
+  'Faqueiro 91 peças':
+    'https://images.unsplash.com/photo-1607082350899-7e105aa886ae?auto=format&fit=crop&w=800&q=80',
+  'Jogo de cama':
+    'https://drive.google.com/file/d/144ABHpj2Ec96XahsLvj3a4k84wzbnFLG/view?usp=sharing',
+  'Jogo de toalhas':
+    'https://drive.google.com/file/d/1iF2TsrwX3YPoTiWyb5AOSnr5JY_eoIVJ/view?usp=sharing',
+  'Passeio de barco':
+    'https://drive.google.com/file/d/1ZFCIGwA_2eXCuta8Xj8z5eZmlua6nnnm/view?usp=sharing',
+  'Spa para casal':
+    'https://drive.google.com/file/d/1PKkbR91jPCtckJ0gSvKk2mPopFneGBwz/view?usp=sharing',
+  Passagens:
+    'https://drive.google.com/file/d/1bQiBR7vVmz9wp2qAoe4pi1O-7DwwcKB5/view?usp=sharing',
+  'Diária de hotel':
+    'https://drive.google.com/file/d/1MIlAUL87_XsixaQc2UpdbNkoirb2Srcd/view?usp=sharing',
+  'Jantar romântico':
+    'https://drive.google.com/file/d/1csUpdoGuhddbX9vfU2xlfP_M2-MLOO_x/view?usp=sharing',
 };
-const cardImage = (nome) => imagensPorProduto[nome] || imagemFallback;
-const PRECO_FIXO = 'R$ 200,00';
+
+function urlDriveParaImagem(url) {
+  if (!url || typeof url !== 'string') return '';
+  if (!url.includes('drive.google.com')) return url;
+
+  const matchById = url.match(/[?&]id=([^&]+)/);
+  const matchByPath = url.match(/\/file\/d\/([^/]+)/);
+  const fileId = matchById?.[1] || matchByPath?.[1];
+
+  if (!fileId) return url;
+  return `https://drive.google.com/thumbnail?id=${fileId}&sz=w1200`;
+}
+
+const cardImage = (nome) => urlDriveParaImagem(imagensPorProduto[nome]) || imagemFallback;
+const formatarPreco = (valor) =>
+  `R$ ${Number(valor).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 
 let presenteSelecionado = '';
 let valorSelecionado = '';
@@ -158,17 +138,18 @@ function abrirModalPix() {
 function montarCards() {
   produtosContainer.innerHTML = '';
 
-  catalogo[categoriaAtual].forEach(([nome]) => {
+  catalogo[categoriaAtual].forEach(([nome, valor]) => {
+    const valorFormatado = formatarPreco(valor);
     const article = document.createElement('article');
     article.className = 'produto';
     article.dataset.produto = nome;
-    article.dataset.valor = PRECO_FIXO;
+    article.dataset.valor = valorFormatado;
 
     article.innerHTML = `
       <img src="${cardImage(nome)}" alt="${nome}" loading="lazy" />
       <h3>${nome}</h3>
       <p>${categoriaAtual}</p>
-      <strong>${PRECO_FIXO}</strong>
+      <strong>${valorFormatado}</strong>
     `;
 
     const imagem = article.querySelector('img');
